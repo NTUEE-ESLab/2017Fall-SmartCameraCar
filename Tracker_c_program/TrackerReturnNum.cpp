afa67d7ddca3234514d4e6bf20c23b7729467dcb
kaoben2731 <b03901004@ntu.edu.tw>
2017-12-27 17:06:13 +0800

    add the basic tracker code

---
+++ Tracker_c_program/TrackerReturnNum.cpp
@@ -0,0 +1,136 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/tracking.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <cstring>
+ 
+using namespace cv;
+using namespace std;
+ 
+// Convert to string
+#define SSTR( x ) static_cast< std::ostringstream & >( \
+( std::ostringstream() << std::dec << x ) ).str()
+ 
+int main(int argc, char **argv)
+{
+    // List of tracker types in OpenCV 3.2
+    // NOTE : GOTURN implementation is buggy and does not work.
+    string trackerTypes[6] = {"BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN"};
+    // vector <string> trackerTypes(types, std::end(types));
+ 
+    // Create a tracker
+    string trackerType = trackerTypes[4];
+ 
+    Ptr<Tracker> tracker;
+    // Tracker* tracker;
+ 
+    #if (CV_MINOR_VERSION < 3)
+    {
+        tracker = Tracker::create(trackerType);
+    }
+    #else
+    {
+        if (trackerType == "BOOSTING")
+            tracker = TrackerBoosting::create();
+        if (trackerType == "MIL")
+            tracker = TrackerMIL::create();
+        if (trackerType == "KCF")
+            tracker = TrackerKCF::create();
+        if (trackerType == "TLD")
+            tracker = TrackerTLD::create();
+        if (trackerType == "MEDIANFLOW")
+            tracker = TrackerMedianFlow::create();
+        if (trackerType == "GOTURN")
+            tracker = TrackerGOTURN::create();
+    }
+    #endif
+    // Read video
+    // VideoCapture video("video-1509343693.mp4");
+    VideoCapture video(0);
+     
+    // Exit if video is not opened
+    if(!video.isOpened())
+    {
+        cout << "Could not read video file" << endl;
+        return 1;
+         
+    }
+
+    video.set(3,320);
+    video.set(4,240);
+    // video.set(5,30);
+     
+    // Read first frame
+    Mat frame;
+    bool ok = video.read(frame);
+     
+    // Define initial boundibg box
+    Rect2d bbox(287, 23, 86, 320);
+     
+    // Uncomment the line below to select a different bounding box
+    bbox = selectROI(frame, false);
+ 
+    // Display bounding box.
+    rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+    // imshow("Tracking", frame);
+     
+    tracker->init(frame, bbox);
+   
+    int FrameCounter=0;
+
+    // while(video.read(frame))
+    while(1)
+    {     
+        video>>frame;
+        // Start timer
+        double timer = (double)getTickCount();
+
+        // Update the tracking result
+        bool ok = tracker->update(frame, bbox);
+        // tracker->update(frame, bbox);;
+
+        // Calculate Frames per second (FPS)
+        float fps = getTickFrequency() / ((double)getTickCount() - timer);
+         
+        if (ok)
+        {
+            // Tracking success : Draw the tracked object
+            rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+            cout<<"\rfps="<<int(fps)<<" x="<<int(bbox.x+bbox.width/2)<<" y="<<int(bbox.y+bbox.height/2)<<" htight="<<int(bbox.height)<<" width="<<int(bbox.width)<<"     ";
+            cout.flush();
+        }
+        else
+        {
+            // Tracking failure detected.
+            // putText(frame, "Tracking failure detected", Point(100,80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
+            cout<<"\rTracking failure detected            ";
+            cout.flush();
+        }
+         
+        // Display tracker type on frame
+        // putText(frame, trackerType + " Tracker", Point(100,20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50),2);
+        
+	FrameCounter++;
+
+       	if(FrameCounter%90==0)
+	{
+        	// Display FPS on frame
+	        putText(frame, "FPS : " + SSTR(int(fps)), Point(100,50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
+
+	        // Display frame.
+	        imshow("Tracking", frame);
+	}
+         
+        // Exit if ESC pressed.
+        int k = waitKey(1);
+        if(k == 27)
+        {
+            cout<<endl;
+            break;
+        }
+    }
+    return 0;
+}
