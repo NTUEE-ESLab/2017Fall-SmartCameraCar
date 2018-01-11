8a5c264d96f1c68e49b61889fb7608b7e5b0ef71
kaoben2731 <b03901004@ntu.edu.tw>
2018-01-11 22:33:32 +0800

    debug

---
+++ flask-video-streaming/myapp_opencv.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
+
+# import camera driver
+# if os.environ.get('CAMERA'):
+#     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+# else:
+    # from camera import Camera
+
+# from camera_opencv import Camera
+import cv2
+
+# Raspberry Pi camera module (requires picamera package)
+# from camera_pi import Camera
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+def gen():
+    """Video streaming generator function."""
+    camera = cv2.VideoCapture(0)
+    if not camera.isOpened():
+            raise RuntimeError('Could not start camera.')
+    while True:
+        _, img = camera.read()
+        frame = cv2.imencode('.jpg', img)[1].tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
