61e5b53050eb85f6fd91f3a9c000917d8f6f65a0
warmtub <b03901162@ntu.edu.tw>
2018-01-13 11:32:33 +0800

    update arduino code

--- Base_motor_and_Plat_motor_controller/Base_motor_and_Plat_motor_controller.ino
+++ Base_motor_and_Plat_motor_controller/Base_motor_and_Plat_motor_controller.ino
@@ -10,8 +10,10 @@ int inputDirection = 0;
 int inputVerticalSpeed = 0;
 int inputHorizontalSpeed = 0;
 int inputCount = 0;
+int inputMode = 0;
 int forward_backward = 0;
 int right_left = 0;
+bool first = true;
 
 //L298n
 // motor one
@@ -27,20 +29,31 @@ int enB = 5;
 Servo myservo;
 
 //Define Variables we'll be connecting to
-double Setpoint, Input, Output;
-float Kp = 0.1;
-float Ki = 0.0;
-float Kd = 0.1;
+//double Setpoint, Input, Output;
+//float Kp = 0.5;
+//float Ki = 0.0;
+//float Kd = 0.0;
 
 //PID
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);;
+//PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);;
 
 //parameters
-int degree = 90;
-int deltaSpeed = 40;
-int moveSpeed = 150;
-int rotateSpeed = 90;
-int servoSpeed = 30;
+//motor
+int minmoveSpeed = 140;
+int moveSpeed = 140;
+int minrotateSpeed = 90;
+int rotateSpeed = 110;
+int deltaSpeed = 10;
+int SurroundmoveSpeed = 120;
+int SurrounddeltaSpeed = 70;
+//threshold
+int verticalThreshold = 100;
+int horizontalThreshold = 100;
+
+//servo
+int degree = 100;
+int servoSpeed = 100;
+
 
 
 void setup()
@@ -63,19 +76,19 @@ void setup()
   myservo.attach(12);
   
   //pid
-  Setpoint = 1;  //dont care in our case
-  Input = 0;
-  myPID.SetMode(AUTOMATIC);
+  //Setpoint = 0;  //dont care in our case
+  //Input = 0;
+  //myPID.SetMode(AUTOMATIC);
 }
 
 // callback for received data
 void receiveData(int byteCount){
   while(Wire.available()) {
     int inputData = Wire.read();
-    Serial.print(inputCount);
-    Serial.print(' ');
-    Serial.print(inputData);
-    Serial.println();
+    //Serial.print(inputCount);
+    //Serial.print(' ');
+    //Serial.print(inputData);
+    //Serial.println();
     
     if (inputData == 255)
     {
@@ -89,9 +102,10 @@ void receiveData(int byteCount){
       inputDirection = input[1];
       inputVerticalSpeed = input[2];
       inputHorizontalSpeed = input[3];
-      forward_backward = (inputDirection)/3;
+      inputMode = (inputDirection)/9;
+      forward_backward = ((inputDirection)/3)%3;
       right_left = (inputDirection)%3;
-      Serial.println();
+      first = true;
     }
   }
 }
@@ -99,65 +113,97 @@ void hardstop()
 {
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
-  analogWrite(enA, 0);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
+  analogWrite(enA, 0);
   analogWrite(enB, 0);
 }
 void backward()
 {
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
-  analogWrite(enA, moveSpeed-deltaSpeed);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
+  analogWrite(enA, moveSpeed-deltaSpeed);
   analogWrite(enB, moveSpeed);
 }
 void forward()
 {
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
-  analogWrite(enA, moveSpeed-deltaSpeed);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
+  analogWrite(enA, moveSpeed-deltaSpeed);
   analogWrite(enB, moveSpeed);
 }
 void left()
 {
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
-  // set speed to 200 out of possible range 0~255
-  analogWrite(enA, rotateSpeed);
-  // turn on motor B
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
-  // set speed to 200 out of possible range 0~255
+  analogWrite(enA, rotateSpeed);
   analogWrite(enB, rotateSpeed);
 }
 void right()
 {
-  // this function will run the motors in both directions at a fixed speed
-  // turn on motor A
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
-  // set speed to 200 out of possible range 0~255
-  analogWrite(enA, rotateSpeed);
-  // turn on motor B
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  // set speed to 200 out of possible range 0~255
+  analogWrite(enA, rotateSpeed);
   analogWrite(enB, rotateSpeed);
+}
+void surround ()
+{
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, moveSpeed-deltaSpeed);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enB, moveSpeed+SurrounddeltaSpeed);
 }
 void loop()
 {
-    myservo.write(97);
+  if (inputMode == 0) // follow mode
+  {
+    myservo.write(98);
+    //moveSpeed = (inputVerticalSpeed < verticalThreshold)? minmoveSpeed+(255-minmoveSpeed)*inputVerticalSpeed/verticalThreshold : 255;
+    //rotateSpeed = (inputHorizontalSpeed < horizontalThreshold)? minrotateSpeed+(255-minrotateSpeed)*inputHorizontalSpeed/horizontalThreshold : 255;
+    //Serial.print(inputDirection);
+    //Serial.print(' ');
+    //Serial.print(moveSpeed);
+    //Serial.print(' ');
+    //Serial.print(rotateSpeed);
+    //Serial.println();
     
+    //i don't know what is this part doning, either
     if (right_left == 1)
     {
+      if (first)
+      {
+        for (int i = 0; i < rotateSpeed; ++i)
+        {
+          analogWrite(enA, i);
+          analogWrite(enB, i);
+          delay (10);
+        }
+      }
+      first = false;
       left ();
     }
     else if (right_left == 2)
     {
+      if (first)
+      {
+        for (int i = 0; i < rotateSpeed; ++i)
+        {
+          analogWrite(enA, i);
+          analogWrite(enB, i);
+          delay (10);
+        }
+      }
+      first = false;
       right ();
     }
     else if (forward_backward == 1)
@@ -172,4 +218,20 @@ void loop()
     {
       hardstop();
     }
+  }
+  else if (inputMode == 1) // surround mode
+  {
+    
+    myservo.write(8);
+    //surround();
+  }
+  else if (inputMode == 2) // 360 mode
+  {
+    rotateSpeed = minrotateSpeed;
+    right ();
+  }
+  else
+  {
+    hardstop();
+  }
 }
